c59c844313f1f65de4c96cea72ffbdc4a4fa6c9f
Karolina Kozubik <k4aro0@gmail.com>
2024-05-08 14:48:26 +0200

    remove code that was left after display separation

--- data_collector.py
+++ data_collector.py
@@ -79,16 +79,6 @@ def data_collector(no_save: bool = True, no_telem: bool = False, no_display=Fals
                 cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET
             )
 
-            depth_colormap_dim = depth_colormap.shape
-            color_colormap_dim = color_image.shape
-
-            if depth_colormap_dim != color_colormap_dim:
-                color_image = cv2.resize(
-                    color_image,
-                    dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
-                    interpolation=cv2.INTER_AREA,
-                )
-
             if not no_telem:
                 telem_data = telemetry.get_telem_data()
                 print(telem_data)
