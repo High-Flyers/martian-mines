1de04efe3dbde03d72075df29bf6b0949ab59b35
RocketFan <kubal123.tomczak@gmail.com>
2024-06-05 16:31:08 +0000

    feat: return home after mission

--- src/mission_controller.py
+++ src/mission_controller.py
@@ -6,6 +6,7 @@ from std_msgs.msg import Empty
 from std_srvs.srv import Trigger
 from typing import List
 from vision_msgs.msg import BoundingBox2D
+from geometry_msgs.msg import PoseStamped
 
 from drone.offboard import Offboard
 from martian_mines.msg import FigureMsgList, FigureMsg, BoundingBoxLabeledList
@@ -13,11 +14,13 @@ from martian_mines.msg import FigureMsgList, FigureMsg, BoundingBoxLabeledList
 
 class States(Enum):
     IDLE = auto()
+    INIT = auto()
     TAKEOFF = auto()
     SCANNING = auto()
     COLLECT_FIGURES = auto()
     TARGET_FIGURE = auto()
     FIGURE_LANDING = auto()
+    RETURN = auto()
 
 
 class MissionController(Machine):
@@ -27,6 +30,7 @@ class MissionController(Machine):
         self.offboard = Offboard()
         self.target_figures: List[FigureMsg] = None
         self.bboxes = BoundingBoxLabeledList()
+        self.local_home_pose = PoseStamped()
 
         self.takeoff_height = rospy.get_param('~takeoff_height', 4)
         self.target_figure_types = rospy.get_param('~target_figure_types', ['blueBall', 'redBall', 'purpleBall'])
@@ -52,17 +56,25 @@ class MissionController(Machine):
 
     def __init_state_machine(self):
         transitions = [
-            ['takeoff', [States.IDLE, States.FIGURE_LANDING], States.TAKEOFF],
+            ['init', States.IDLE, States.INIT],
+            ['takeoff', [States.INIT, States.FIGURE_LANDING], States.TAKEOFF],
             {'trigger': 'takeoff_finished', 'source': States.TAKEOFF, 'dest': States.SCANNING, 'conditions': 'is_need_scanning'},
             ['collect_figures', States.SCANNING, States.COLLECT_FIGURES],
-            ['target_figure', [States.SCANNING, States.TAKEOFF], States.TARGET_FIGURE],
+            ['target_figure', States.SCANNING, States.TARGET_FIGURE],
+            ['takeoff_finished', States.TAKEOFF, States.TARGET_FIGURE],
             ['land_figure', States.TARGET_FIGURE, States.FIGURE_LANDING],
+            ['return_home', States.TARGET_FIGURE, States.RETURN]
         ]
 
         Machine.__init__(self, states=States, transitions=transitions, initial=States.IDLE)
 
     def is_need_scanning(self):
         return self.target_figures is None
+    
+    def on_enter_INIT(self):
+        self.local_home_pose = self.offboard.local_pos
+
+        self.takeoff()
 
     def on_enter_TAKEOFF(self):
         self.timer_takeoff = rospy.Timer(rospy.Duration(0.02), self.cb_timer_takeoff)
@@ -82,7 +94,7 @@ class MissionController(Machine):
         self.client_generate_trajectory()
         self.client_figure_finder_start()
 
-    def on_enter_TARGET_FIGURE(self, target_figures: List[FigureMsg]):
+    def on_enter_TARGET_FIGURE(self):
         def cb_timer_fly_to_figure(figure: FigureMsg):
             figure_point = [figure.local_x, figure.local_y, self.target_figure_approach_height]
             self.offboard.fly_point(*figure_point)
@@ -92,13 +104,17 @@ class MissionController(Machine):
                 self.land_figure(target_figure)
                 self.cb_timer_fly_to_figure.shutdown()
 
-        target_figure = target_figures.pop()
+        if len(self.target_figures) == 0:
+            self.return_home()
+            return
+
+        target_figure = self.target_figures.pop()
         self.cb_timer_fly_to_figure = rospy.Timer(rospy.Duration(0.02), lambda _: cb_timer_fly_to_figure(target_figure))
         rospy.sleep(0.2)
         self.offboard.set_offboard_mode()
 
     def on_enter_FIGURE_LANDING(self, target_figure: FigureMsg):
-        def cb_figure_landing(_, target_figure: FigureMsg):
+        def cb_figure_landing(target_figure: FigureMsg):
             bboxes = self.bboxes.boxes
 
             if len(bboxes) == 0:
@@ -109,6 +125,18 @@ class MissionController(Machine):
 
         self.timer_figure_landing = rospy.Timer(rospy.Duration(0.02), lambda _: cb_figure_landing(target_figure))
         self.client_precision_landing_start()
+    
+    def on_enter_RETURN(self):
+        def cb_timer_return(event):
+            self.offboard.fly_point(self.local_home_pose.pose.position.x, self.local_home_pose.pose.position.y, self.takeoff_height)
+
+            if self.offboard.is_point_reached(self.local_home_pose.pose.position.x, self.local_home_pose.pose.position.y, self.takeoff_height):
+                self.offboard.land()
+                self.event.shutdown()
+
+        rospy.Timer(rospy.Duration(0.02), lambda event: cb_timer_return(event))
+        rospy.sleep(0.2)
+        self.offboard.set_offboard_mode()
 
     def cb_precision_landing_finished(self, _):
         self.timer_figure_landing.shutdown()
@@ -121,7 +149,7 @@ class MissionController(Machine):
         target_figures = [fig for fig in msg.figures if fig.type in self.target_figure_types]
         target_figures.sort(key=lambda fig: self.target_figure_types.index(fig.type))
         self.target_figures = target_figures[::-1]
-        self.target_figure(self.target_figures)
+        self.target_figure()
 
     def cb_detection_bboxes(self, msg: BoundingBoxLabeledList):
         self.bboxes = msg
@@ -132,7 +160,7 @@ if __name__ == "__main__":
 
     try:
         mission_controller = MissionController()
-        mission_controller.takeoff()
+        mission_controller.init()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
