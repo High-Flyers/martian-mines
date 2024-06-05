3a6fd8473d41fc9c138483ed336d7e4ee7e687b1
RocketFan <kubal123.tomczak@gmail.com>
2024-06-05 16:30:30 +0000

    feat: transform trajectory from start_pose

--- src/trajectory_generator.py
+++ src/trajectory_generator.py
@@ -1,25 +1,39 @@
 import rospy
 import matplotlib
 
-from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import CameraInfo
 from image_geometry import PinholeCameraModel
 from nav_msgs.msg import Path
 from std_srvs.srv import Trigger, TriggerResponse
+from tf2_ros import Buffer, TransformListener
+from tf2_geometry_msgs import PoseStamped
+
 from drone.scan_trajectory import ScanTrajectory
 from utils.environment import Environemnt
 
 
 class TrajectoryGenerator():
     def __init__(self):
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer)
+
+        self.trajectory_link = rospy.get_param('~trajectory_link', 'start_pose')
+
         camera_info = rospy.wait_for_message('camera/camera_info', CameraInfo, timeout=3)
+
         self.camera_model = PinholeCameraModel()
         self.camera_model.fromCameraInfo(camera_info)
         self.scan_trajectory = self.create_scan_trajectory()
 
+        self.wait_for_transform()
         self.service_generate = rospy.Service('trajectory_generator/generate', Trigger, self.generate_trajectory)
-        self.pub_trajectory = rospy.Publisher('trajectory_generator/path', Path, queue_size=1, latch=True)
-    
+        self.pub_trajectory = rospy.Publisher('trajectory_generator/path', Path, queue_size=1)
+
+    def wait_for_transform(self):
+        while not rospy.is_shutdown():
+            if self.tf_buffer.can_transform(self.trajectory_link, "map", rospy.Time(0), rospy.Duration(1.0)):
+                break
+
     def create_scan_trajectory(self) -> ScanTrajectory:
         altitude = float(rospy.get_param('~altitude', 4))
         overlap = float(rospy.get_param('~overlap', 0.1))
@@ -44,10 +58,14 @@ class TrajectoryGenerator():
 
         for waypoint in waypoints:
             pose = PoseStamped()
+            pose.header.stamp = rospy.Time.now()
+            pose.header.frame_id = self.trajectory_link
             pose.pose.position.x = waypoint[0]
             pose.pose.position.y = waypoint[1]
             pose.pose.position.z = waypoint[2]
-            path.poses.append(pose)
+            pose_map_link = self.tf_buffer.transform(pose, "map")
+
+            path.poses.append(pose_map_link)
 
         self.pub_trajectory.publish(path)
 
