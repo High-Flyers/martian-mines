837e52012f9eafff6030444bab1e139ebcab4ebd
Karolina Kozubik <k4aro0@gmail.com>
2024-06-05 12:10:25 +0200

    velocity traking

--- src/barrel_tracker.py
+++ src/barrel_tracker.py
@@ -1,7 +1,7 @@
 import rospy
 import numpy as np
 
-from std_srvs.srv import Trigger
+from std_srvs.srv import Trigger, TriggerResponse
 from std_msgs.msg import Empty
 from sensor_msgs.msg import CameraInfo
 
@@ -13,10 +13,11 @@ from utils.environment import Environment
 
 class BarrelTracker:
     def __init__(self, alt=4):
-        rospy.set_param("~detector", "aruco")
+        rospy.set_param("~detector", "yolo")
 
         self.offboard = Offboard()
-        self.camera_info = rospy.wait_for_message("camera/camera_info", CameraInfo)
+        camera_info = rospy.wait_for_message("camera/camera_info", CameraInfo)
+        self.camera_center = np.array([camera_info.width, camera_info.height]) * 0.5
 
         self.barrel_waypoint = (*Environment(0, 0).barrel, alt)
 
@@ -24,10 +25,13 @@ class BarrelTracker:
         self.finished_pub = rospy.Publisher("barrel_tracker/finished", Empty, queue_size=1)
         self.bboxes_sub = rospy.Subscriber("detection/bboxes", BoundingBoxLabeledList, callback=self.detection_callback)
 
+        rospy.wait_for_service("figure_finder/finish")
+
     def activate(self, _):
         self.point_timer = rospy.Timer(rospy.Duration(0.02), callback=self.point_timer_callback)
         rospy.sleep(0.2)
         self.offboard.set_offboard_mode()
+        return TriggerResponse(success=True)
 
     def point_timer_callback(self, _):
         self.offboard.fly_point(*self.barrel_waypoint)
@@ -35,23 +39,32 @@ class BarrelTracker:
         if self.offboard.is_point_reached(*self.barrel_waypoint):
             self.offboard.set_hold_mode()
             self.point_timer.shutdown()
+            rospy.wait_for_service("figure_finder/start")
 
     def detection_callback(self, data):
         for bbox in data.boxes:
-            if bbox.label != 0:
+            if bbox.label != "Barrel":
                 continue
-            self.diff_vector = np.array([self.camera_info.width, self.camera_info.height]) - bbox.center
+            bbox = bbox.bbox
+            self.diff_vector = self.camera_center - np.array([bbox.center.x, bbox.center.y])
             self.velocity_timer = rospy.Timer(rospy.Duration(0.02), callback=self.velocity_timer_callback)
             rospy.sleep(0.2)
             self.offboard.set_offboard_mode()
 
     def velocity_timer_callback(self, _):
-        self.offboard.fly_velocity(*self.diff_vector, vz=0)
+        velocities = self.get_velocities(self.diff_vector)
+        self.offboard.fly_velocity(*velocities, vz=0)
 
         if np.allclose(self.diff_vector, [0, 0]):
             self.offboard.set_hold_mode()
             self.point_timer.shutdown()
             self.finished_pub.publish()
+            rospy.wait_for_service("figure_finder/finish")
+
+    def get_velocities(self, target_vector):
+        distance = np.linalg.norm(target_vector)
+        velocity_factor = -2 ** (-(distance) + 1) + 2
+        return target_vector * velocity_factor / distance
 
 
 if __name__ == "__main__":
